6021f8bc78bdac573b52f3ab850576d11e655d26
Jan Schneider <jan@horde.org>
2017-10-12 17:07:10 +0200

    Migrated from framework/ to split repositories.

--- test/Horde/LoginTasks/Autoload.php
+++ test/Horde/LoginTasks/Autoload.php
@@ -6,7 +6,6 @@
  * @package  LoginTasks
  * @author   Gunnar Wrobel <wrobel@pardus.de>
  * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
- * @link     http://pear.horde.org/index.php?package=LoginTasks
  */
 
 require_once __DIR__ . '/Stubs.php';
